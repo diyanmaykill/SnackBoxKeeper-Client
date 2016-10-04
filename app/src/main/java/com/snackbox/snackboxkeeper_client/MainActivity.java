1b8587cd15360be75a2120ae5b8494abb28f18d8
Jan Michael Raymundo <j.raymundo@klab.com>
2016-10-04 14:23:30 +0800

    Added first activity

---
+++ app/src/main/java/com/snackbox/snackboxkeeper_client/MainActivity.java
@@ -0,0 +1,15 @@
+package com.snackbox.snackboxkeeper_client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jan Michael Raymundo on 10/4/16.<br> Copyright © 2016 KlabCyscorpions. All rights reserved.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
